1876ba0f3f4c2693cff168142066bc0958e9c7eb
Prince Chukwudire <chimeremezechukwudire@gmail.com>
2020-03-25 22:25:59 +0100

    initial commit

---
+++ task1.js
@@ -0,0 +1,14 @@
+//Javascript Task 1
+
+let name = 'Prince Chukwudire'
+courses = ['Html', 'Css', 'Javascript', 'Php'];
+
+console.log('My name is' + ' ' + name + ' ' + 'and i offer ' + ' ' + courses)
+
+//courses total 3 hence;
+
+for (let i = 0; i <= 200; i++) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
